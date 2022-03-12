96761a69364a2df21718f50c71f6242f2636c7ea
brennamehl <brennam@email.unc.edu>
2022-03-12 13:47:48 -0500

    added flips.js functionality

--- flips.js
+++ flips.js
@@ -1,11 +1,6 @@
 import { coinFlip, coinFlips } from "./modules/coin.mjs";
-const args = require('minimist')(process.argv.slice(2))
+import minimist from "minimist";
+const args = minimist(process.argv.slice(2))
 args['number']
-const num = args.number
-console.log(num)
-if(num>=1){
-    console.log(coinFlips(num))
-}
-else{
-    console.log(coinFlips(1))
-}
+const num = args.number || 1
+console.log(coinFlips(num))
